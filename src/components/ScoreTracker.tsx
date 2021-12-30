68dde623997a719d107ecb7e683ae3bebef50298
Daan Brocatus <daan.brocatus@outlook.com>
2021-12-30 13:40:48 +0100

    Add calculator interface

---
+++ src/components/ScoreTracker.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './ScoreTracker.scss';
+import Calculator from "./Calculator";
+
+function ScoreTracker() {
+    return (
+        <div className="scoreTrackerContainer">
+            <Calculator />
+        </div>
+    )
+}
+
+export default ScoreTracker;
